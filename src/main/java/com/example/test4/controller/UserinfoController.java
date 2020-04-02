be85707998f769f505ecfb71e6d565b66b2ee389
hanweiwoniuxy <hanwei@woniuxy.com>
2020-04-02 09:54:02 +0800

    insert:UsrinfoController

--- src/main/java/com/example/test4/controller/UserinfoController.java
+++ src/main/java/com/example/test4/controller/UserinfoController.java
@@ -1,4 +1,12 @@
 package com.example.test4.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserinfoController {
+    @GetMapping("say")
+    public String say(){
+        return "hello";
+    }
 }
